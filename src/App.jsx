8b8080c6d3739bf12254a0ca1390126618842870
Youssef Moti <youssefmoti@yahoo.com>
2019-11-13 10:26:09 +0100

    Create todos compnent

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import style from './App.module.scss';
+import React from "react";
+import style from "./App.module.scss";
+import { TodosList } from "./Components/Todos/TodosList";
 
 function App() {
   return (
     <div className={style.App}>
-      <header className={style.AppHeader}>
-        <img src={logo} className={style.AppLogos} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={style.AppLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodosList />
     </div>
   );
 }
