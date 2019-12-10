533909cac9d27a939a9aab3fdc396efbd607e8b3
Youssef Moti <youssefmoti@yahoo.com>
2019-12-10 23:41:38 +0100

    Add routers packages and configure store

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,11 +1,21 @@
-import {
-    createStore
-} from "redux";
-import {
-    todosReducer
-} from "./Reducers/todos";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import reduxThunk from "redux-thunk";
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import { createBrowserHistory } from "history";
+import { todosReducer } from "./Reducers/todos";
+
+export const history = createBrowserHistory();
+
+const middleware = [reduxThunk, routerMiddleware(history)];
 
 export const store = createStore(
+  combineReducers({
     todosReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    router: connectRouter(history)
+  }),
+  composeWithDevTools(
+    applyMiddleware(...middleware)
+    // other store enhancers if any
+  )
 );
