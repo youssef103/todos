52b8c51190bd3223527a1c18254fc1db306136c8
Youssef Moti <youssefmoti@yahoo.com>
2019-12-18 20:18:13 +0100

    Mock api service

--- src/Services/__mocks__/api.js
+++ src/Services/__mocks__/api.js
@@ -1,8 +1,15 @@
 import {
     todos
 } from '../../../fixture/todos'
+
+const getAll= async ()=>{
+    return await new Promise((resolve, reject)=> {
+        resolve([...todos]);
+        reject({message: "No Data"})
+    })
+}
+
+
 export default {
-    getAll : () => {
-        return Promise.resolve([...todos])
-    }
+    getAll
 }
--- src/Store/Actions/todos.test.jsx
+++ src/Store/Actions/todos.test.jsx
@@ -12,20 +12,45 @@ import {
   fetchTodosFailure
 } from "./todos";
 import { todos } from "../../../fixture/todos";
-//import api from '../../Services/api.js'
+import api from "../../Services/api.js";
 
-jest.mock("../../Services/api.js");
+//jest.mock("../../Services/api.js");
+api.getAll = jest.fn();
 
 describe("Test async action", () => {
   const createMockStore = configureMockStore([thunk]);
 
   test("should fetch todos correctly", async () => {
     const store = createMockStore({});
+    //await api.getAll.mockImplementation(()=>  Promise.resolve(todos));
+    //const myMock= await api.getAll.mockReturnValueOnce(Promise.resolve(todos));
+    //const myMock= api.getAll.mockImplementation(()=> Promise.resolve(error));
+    const myMock = api.getAll.mockResolvedValue(todos);
+
     await store.dispatch(getTodos());
     const actions = store.getActions();
 
     expect(actions[0]).toEqual(fetchTodos());
+
+    expect(myMock.call.length).toBe(1);
     expect(actions[1]).toEqual(fetchTodosSuccess(todos));
+  });
+
+  test("should Not fecth todos", async () => {
+    const store = createMockStore({});
+    const error = { message: "No have data" };
+    //const myMock= await api.getAll.mockReturnValueOnce(Promise.reject(error));
+    //const myMock=api.getAll.mockImplementation(()=> Promise.reject(error));
+    const myMock = api.getAll.mockRejectedValue(error);
+
+    await store.dispatch(getTodos());
+    const actions = store.getActions();
+
+    expect(actions[0]).toEqual(fetchTodos());
+
+    //expect(actions[1]).rejects.toEqual(fetchTodosFailure('error'));
+    expect(myMock.call.length).toBe(1);
+    expect(actions[1]).toEqual(fetchTodosFailure(error));
   });
 });
 
